7d02e16cb5c0179d2a960071d738c6f522714b32
Tomasz Szatkowski <szatkus@gmail.com>
2020-08-01 21:52:41 +0200

    0.9.1 Add support for Django 2.x

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+.. image:: https://img.shields.io/pypi/v/django-partial-content.svg
+  :target: https://pypi.python.org/pypi/django-partial-content/
+
 Support for partial content in Django staticfiles.
 
 After an installation add the following app to your settings.
@@ -12,7 +15,7 @@ After an installation add the following app to your settings.
 
 After that Django will respect `Range` header in requests to static files. Note that using `staticfiles` module in production is not recommended. The extension is only for ease of development.
 
-Tested with Django 3.0, SRWare Iron and files no larger than 40MB.
+Tested with Django (2.0, 3.0), SRWare Iron and files no larger than 40MB.
 
 Known issues:
  - Multiple ranges aren't supported.
--- src/__init__.py
+++ src/__init__.py
@@ -19,8 +19,8 @@ django.views.static.serve = handler
 
 
 def handle(request: HttpRequest, response: HttpResponse):
-    if 'range' in request.headers and response.status_code == 200:
-        header = request.headers['range']
+    if request.META.get('HTTP_RANGE') and response.status_code == 200:
+        header = request.META['HTTP_RANGE']
         if header.find('bytes=') == 0:
             splitter = re.compile('[-,]')
             parts = splitter.split(header[6:].strip())
@@ -32,7 +32,6 @@ def handle(request: HttpRequest, response: HttpResponse):
             response._set_streaming_content(io.BufferedReader(io.BytesIO(content[start:end])))
             response._headers['content-length'] = 'Content-Length', str(end - start)
             response._headers['content-range'] = 'Content-Range', 'bytes %d-%d/%d' % (start, end - 1, len(content))
-            del response._headers['content-disposition']
             response.status_code = 206
 
     return response
